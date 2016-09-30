289af55048d8fbfce81a19532f032e8dbed0ec2d
anlijudavid <anlijudavid@hotmail.com>
2016-09-30 00:50:48 -0500

    nuevo look pagina principal

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -1,23 +1,39 @@
 import { Component } from '@angular/core';
 
 import { NavController, AlertController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 import { BarcodeScanner } from 'ionic-native';
 
 @Component({
   selector: 'page-page1',
-  templateUrl: 'page1.html'
+  templateUrl: 'page1.html',
+  providers: [
+    [Storage]
+  ]
 })
 export class Page1 {
 
+  count: number = 0;
+
   constructor(
     public navCtrl: NavController,
-    public alertCtrl: AlertController
+    public alertCtrl: AlertController,
+    public storage: Storage
   ) {
 
   }
 
-  ionViewDidLoad() {    
+  ionViewDidLoad() {
+    this.storage.get('count').then((value: number) => {
+      if (value || value === 0) {
+        console.log("Count: ", value);
+
+        this.count = value;
+      } else {
+        this.storage.set('count', 0);
+      }
+    });
   }
 
   showAlert(title: string, subtitle: string) {
@@ -31,9 +47,17 @@ export class Page1 {
 
   readBarcode() {
     BarcodeScanner.scan().then((barcodedata) => {
-      this.showAlert('Codigo leido correctamente', JSON.stringify(barcodedata));
+      this.showAlert(
+        'Codigo leido correctamente',
+        JSON.stringify(barcodedata)
+      );
     }), (err) => {
       console.log("Error: ", err);
+
+      this.showAlert(
+        'Ha ocurrido un error al leer el codigo!',
+        JSON.stringify(err)
+      );
     };
   }
 
